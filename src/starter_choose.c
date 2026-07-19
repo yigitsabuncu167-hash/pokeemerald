60a76854e1eaf825b7e59df67f740fa09dd98952
yigitsabuncu167-hash <yigitsabuncu167@gmail.com>
2026-07-19 14:42:53 +0300

    Update starter_choose.c

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -112,7 +112,7 @@ static const u8 sStarterLabelCoords[STARTER_MON_COUNT][2] =
 
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
-    SPECIES_AZURILL,
+    SPECIES_MARILL,
     SPECIES_PICHU,
     SPECIES_BULBASAUR,
 };
